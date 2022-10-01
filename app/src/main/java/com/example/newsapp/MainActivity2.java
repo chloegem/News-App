ba07c3ab97701908ac9d9644578faaf23b912cee
chloegem <chloegemayel1@gmail.com>
2022-10-01 17:05:17 +0300

    menu part 1

--- app/src/main/java/com/example/newsapp/MainActivity2.java
+++ app/src/main/java/com/example/newsapp/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        return super.onCreateOptionsMenu(menu);
     }
 }
