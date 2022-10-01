a979dba990bd723582d0ba9ac5591b295946e9a5
chloegem <chloegemayel1@gmail.com>
2022-10-01 12:15:59 +0300

    home screen part 5

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    public void onClick(){
+    public void onClick(View v){
     EditText user = (EditText) findViewById(R.id.user);
-
     String user_val = user.getText().toString();
 
-        Toast.makeText(getApplicationContext(), "Signed In" + user_val, Toast.LENGTH_LONG).show();
-        Intent i = new Intent(getApplicationContext(),MainActivity2.class);
+        Toast.makeText(getApplicationContext(), "Signed In: " + user_val, Toast.LENGTH_LONG).show();
+        Intent i = new Intent(MainActivity.this,MainActivity2.class);
         startActivity(i);
     }
 }
