52f90ac83bc792e9cc2d431a699170cdf282c96f
chloegem <chloegemayel1@gmail.com>
2022-10-01 11:41:10 +0300

    home screen part 3

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(){
+    EditText user = (EditText) findViewById(R.id.user);
+
+    String user_val = user.getText().toString();
+
+        Toast.makeText(getApplicationContext(), "Signed In" + user_val, Toast.LENGTH_LONG).show();
+        Intent i = new Intent(getApplicationContext(),MainActivity2.class);
+        startActivity(i);
     }
 }
