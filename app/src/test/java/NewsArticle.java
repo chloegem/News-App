9f40e6b3cafd5fbed1a69e2a5d02dde53a3f5a65
chloegem <chloegemayel1@gmail.com>
2022-10-02 02:04:26 +0300

    menu part 7

---
+++ app/src/test/java/NewsArticle.java
@@ -0,0 +1,12 @@
+public class NewsArticle {
+
+    String title, author, publisher, location, details;
+
+    public NewsArticle(String title, String author, String publisher, String location, String details) {
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.location = location;
+        this.details = details;
+    }
+}
