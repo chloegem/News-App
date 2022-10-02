fa078676bce541099a036e38005e31808ac04892
chloegem <chloegemayel1@gmail.com>
2022-10-02 17:18:07 +0300

    ada[ter part 4

--- app/src/main/java/com/example/newsapp/MainActivity2.java
+++ app/src/main/java/com/example/newsapp/MainActivity2.java
@@ -2,6 +2,8 @@ package com.example.newsapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,12 +11,30 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.util.List;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    Adapter adapter;
+    List<NewsArticle> newsArticleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        recyclerView = findViewById(R.id.recyclerViewAdd);
+        NewsDatabase newsDatabase = new NewsDatabase(this);
+        newsArticleList = newsDatabase.getNewsArticle(); ////////////CHECK WHERE GET NEWS IS FROM
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new Adapter(this,newsArticleList);
+        recyclerView.setAdapter(adapter);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
